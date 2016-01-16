e4d4947c62468ba648cabad998676c92a1f57932
Yoann Fleury <yoann.fleury@yahoo.com>
2016-01-16 18:30:19 +0100

    feat(user) : add user profile

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Secret Santa is an application that you can host and run on you server to
 do a secret santa with your family and friends.
+
+This application is build on Silex, a micro-framework based on the
+awesome framework Symfony. Check the
+[composer.json](https://github.com/yoannfleurydev/secret-santa/blob/master/composer.json)
+file to see all the dependencies.
--- app/routes/get.php
+++ app/routes/get.php
@@ -15,9 +15,32 @@ $app->get('/login', function () use ($app) {
     return $app['twig']->render('login.html.twig');
 })->bind('login_get');
 
-$app->get('/user/{id}/', function () use ($app) {
-    return $app['twig']->render('user.html.twig');
-});
+$app->get('/user/{id}', function ($id) use ($app) {
+    if (null === $app['session']->get('user')) {
+        $app['session']->getFlashBag()->add(
+            'message',
+            array(
+                'type' => 'danger',
+                'content' => 'Vous devez être connecté pour accéder à ce profil'
+            )
+        );
+        return $app->redirect($app['url_generator']->generate('login_get'));
+    }
+
+    if ($app['session']->get('user')->getUserId() !== $id) {
+        $app['session']->getFlashBag()->add('message', array('type' => 'danger', 'content' => 'Cette opération ne vous est pas permise'));
+        return $app->redirect('/login');
+    }
+
+    $user = $app['dao.user']->find($id);
+
+    return $app['twig']->render('user.html.twig', array('user' => $user));
+})->bind('user')->assert('id', '\d+');
+
+
+
+
+
 
 $app->get('/logout', function () use ($app) {
     $app['session']->clear();
