1521c945948d759e87db0499dffc78fdb281494f
Yoann Fleury <yoann.fleury@yahoo.com>
2016-01-16 13:04:59 +0100

    feat(app) : add logout and beginning of user profile

--- app/routes/get.php
+++ app/routes/get.php
@@ -14,3 +14,20 @@ $app->get('/signup', function () use ($app) {
 $app->get('/login', function () use ($app) {
     return $app['twig']->render('login.html.twig');
 })->bind('login_get');
+
+$app->get('/user/{id}/', function () use ($app) {
+    return $app['twig']->render('user.html.twig');
+});
+
+$app->get('/logout', function () use ($app) {
+    $app['session']->clear();
+
+    $app['session']->getFlashBag()->add('message',
+        array(
+            'type' => 'success',
+            'content' => 'Vous êtes déconnecté'
+        )
+    );
+
+    return $app->redirect($app['url_generator']->generate('index'));
+})->bind('logout');
